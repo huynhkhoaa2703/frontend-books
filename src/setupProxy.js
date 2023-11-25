8de709dc09ca0c194dacbacd68379e5310434667
huynhkhoaa2703 <huynhkhoaa2703@gmail.com>
2023-11-26 00:36:38 +0700

    upload api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:3030",
+      target: "https://backend-o5je.onrender.com",
       changeOrigin: true,
     })
   );
